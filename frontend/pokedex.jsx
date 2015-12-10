a82f4781e7b3c8b8377573af1dceb29e249e360a
David Roth <dneilroth@gmail.com>
2015-12-10 11:18:04 -0800

    initial commit

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+
+document.addEventListener('DOMContentLoaded', function () {
+  var content = document.querySelector('#content');
+  // ReactDOM.render(<Router>{routes}</Router>, content);
+});
